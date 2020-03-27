3e9ed422dd002f012cedb5f852e1e549911f1490
xgerhard <xgerhard@2g.be>
2020-03-27 17:21:55 +0100

    Hide win% on 0 games

--- src/app/Http/Controllers/CommandController.php
+++ src/app/Http/Controllers/CommandController.php
@@ -171,7 +171,7 @@ class CommandController
 
                                         case $x instanceof TrialsReportFireteamReport:
                                             $oFireteamStatReport = $x;
-                                            $strCharacterRes .= '['. $this->formatNoBnet($oFireteamStatReport->displayName) .': Games: '. $oFireteamStatReport->games .' (W'. $oFireteamStatReport->winp .'%) | KD: '. $oFireteamStatReport->kd .' | KA/D: '. $oFireteamStatReport->kda . ($oFireteamStatReport->flawless > 0 ? ' | Flawless: '. $oFireteamStatReport->flawless : '') .'], ';
+                                            $strCharacterRes .= '['. $this->formatNoBnet($oFireteamStatReport->displayName) .': Games: '. $oFireteamStatReport->games . ($oFireteamStatReport->games > 0 ? ' (W'. $oFireteamStatReport->winp .'%)' : '') .' | KD: '. $oFireteamStatReport->kd .' | KA/D: '. $oFireteamStatReport->kda . ($oFireteamStatReport->flawless > 0 ? ' | Flawless: '. $oFireteamStatReport->flawless : '') .'], ';
                                             $bFound = true;
                                         break;
 
